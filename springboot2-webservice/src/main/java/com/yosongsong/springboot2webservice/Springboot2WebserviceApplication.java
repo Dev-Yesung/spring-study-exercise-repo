d3b4d3bcda5259a9194a8a4bc32f04e68b8eb423
ysng_ysng <yeasung67@gmail.com>
2023-01-01 17:08:12 +0900

    챕터4 화면구성까지 끝냈습니다.

---
+++ springboot2-webservice/src/main/java/com/yosongsong/springboot2webservice/Springboot2WebserviceApplication.java
@@ -0,0 +1,14 @@
+package com.yosongsong.springboot2webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class Springboot2WebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2WebserviceApplication.class, args);
+    }
+}
